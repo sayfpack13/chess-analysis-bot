2a4ebde2d9be963e13eb69cb08ec320e6877b389
sayf pack <80095684+sayfpack13@users.noreply.github.com>
2023-02-22 14:24:29 +0100

    Add files via upload

--- utils/path.js
+++ utils/path.js
@@ -9,15 +9,24 @@ const windowsEnginePath = (name) => {
   if (!fs.existsSync(engine_path)) {
     console.log("engine not found: "+name)
     name="stockfish-15.exe"
-    engine_path = path.resolve(process.cwd(), 'utils/engine', "stockfish-15.exe")
+    engine_path = path.resolve(process.cwd(), 'utils/engine', name)
   }
   console.log("using engine: "+name)
   return engine_path
 
 }
 
-const linuxEnginePath = (name = 'stockfish') =>
-  name === 'stockfish' ? '../Stockfish/src/stockfish' : '../engine/komodo13'
+const linuxEnginePath = (name) =>{
+  var engine_path = path.resolve(process.cwd(), 'utils/engine', name)
+ 
+  if (!fs.existsSync(engine_path)) {
+    console.log("engine not found: "+name)
+    name="stockfish-15"
+    engine_path = path.resolve(process.cwd(), 'utils/engine', name)
+  }
+  console.log("using engine: "+name)
+  return engine_path
+}
 
 module.exports = {
   windowsEnginePath,
